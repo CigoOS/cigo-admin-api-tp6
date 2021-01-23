a94bee663216b4698fc706d4b6637578edaf7d0a
zim <zim@cigoos.com>
2021-01-24 02:45:35 +0800

    增加index应用

---
+++ app/index/controller/Index.php
@@ -0,0 +1,12 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\index\controller;
+
+class Index
+{
+    public function index()
+    {
+        return '您好！[index]示例应用。欢迎使用CigoAdmin后台快速搭建框架：Enjoy it！！';
+    }
+}
